8ca68d445b0c83408ba5653d349746799a3815f3
Bre R <2326@holbertonschool.com>
2020-09-17 03:09:14 +0000

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+/**
+* main- void
+* description: prints sizes of various variables on system
+* Return: 0
+*/
+#include <stdio.h>
+int main(void)
+{
+printf("Size of a char: %c\n");
+printf("Size of and int: %d\n");
+printf("Size of a long int: %ld\n");
+printf("Size of a long long int: %lld\n");
+printf("Size of a float: %f\n");
+return (0);
+}
