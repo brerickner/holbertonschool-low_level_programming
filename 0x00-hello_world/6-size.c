d3bb1b652c15a6c572c6ad17dffb5358fad02bb8
Bre R <2326@holbertonschool.com>
2020-09-17 03:47:42 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@ int main(void)
 {
 char a;
 int x;
-long int y:
+long int y;
 long long int z;
 float f;
 
@@ -18,10 +18,10 @@ y = 4;
 z = 8;
 f = 4;
 
-printf("Size of a char: %c\n", a);
-printf("Size of and int: %d\n", x);
-printf("Size of a long int: %ld\n", y);
-printf("Size of a long long int: %llu\n", z);
-printf("Size of a float: %f\n", f);
+printf("Size of a char: %d byte(s)\n", a);
+printf("Size of and int: %d byte(s)\n", x);
+printf("Size of a long int: %ld byte(s)\n", y);
+printf("Size of a long long int: %llu byte(s)\n", z);
+printf("Size of a float: %1.0f byte(s)\n", f);
 return (0);
 }
