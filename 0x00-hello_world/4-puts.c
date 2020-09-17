73d9b8f3d2b8aab0cf7228a8694fbb05ccb00785
Bre R <2326@holbertonschool.com>
2020-09-17 02:07:38 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+/* Description: program to print specified statement using puts function */
+#include <stdio.h>
+        
+int main (void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
