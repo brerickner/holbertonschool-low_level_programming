3ff96b5526d5cf0967f76ced87b8f4b347a599d1
Bre R <2326@holbertonschool.com>
2020-09-17 03:35:46 +0000

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,7 +6,6 @@
 #include <stdio.h>
 int main(void)
 {
-
 char a;
 int x;
 long int y:
@@ -16,13 +15,13 @@ float f;
 a = 1;
 x = 4;
 y = 4;
-z = 16;
+z = 8;
 f = 4;
 
 printf("Size of a char: %c\n", a);
 printf("Size of and int: %d\n", x);
 printf("Size of a long int: %ld\n", y);
-printf("Size of a long long int: %llu\n," z);
+printf("Size of a long long int: %llu\n", z);
 printf("Size of a float: %f\n", f);
 return (0);
 }
