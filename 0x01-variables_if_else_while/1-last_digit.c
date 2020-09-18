b5c33ce36772b92f49edb7f215ae72c1c626024f
Bre R <2326@holbertonschool.com>
2020-09-18 03:34:56 +0000

    added an extra )

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ if (n % 10 > 5)
 {
 printf("and is greater than 5\n");
 }
-if else(n % 10 < 6 && (n % 10 != 0)
+if else(n % 10 < 6 && (n % 10 != 0))
 {
 printf("and is less than 6 and not 0\n");
 }
