c086c040fd4f7707129d7592a2e482fb20c95bc0
Bre R <2326@holbertonschool.com>
2020-09-18 03:25:24 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,18 +13,19 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is %d\n", n, n % 10);
 
 if ((n % 10) > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, (n % 10));
+printf("and is greater than 5\n");
 }
-if else((n % 10) < 6 && (n % 10) != (0))
+if else((n % 10) < 6 && (n % 10) != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10));
+printf("and is less than 6 and not 0\n");
 }
 else
 {
-printf("Last digit of %d is %d and is 0\n", n, (n % 10));
+printf("and is 0\n");
 }
 return (0);
 }
