02c72ae4073bb0f86017b11bd43b83aabaf710b1
Bre R <2326@holbertonschool.com>
2020-09-19 03:20:23 +0000

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+/**
+* main- void
+* description: print single digit base 10
+* Return: 0
+*/
+
+#include <stdio.h>
+int main(void)
+{
+char a = 'z';
+
+for (; a >= 'a'; a--)
+
+putchar(a);
+{
+putchar('\n');
+}
+return (0);
+}
