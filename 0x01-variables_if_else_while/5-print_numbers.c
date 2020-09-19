8022e4b7c0b749d877064f223fa0c2149c49d78d
Bre R <2326@holbertonschool.com>
2020-09-19 01:20:44 +0000

    5-print_numbers.c added source code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/**
+* main- void
+* description: print single digit base 10
+* Return: 0
+*/
+
+#include <stdio.h>
+int main(void)
+{
+int x = 0;
+
+for (; x <= 10; x++)
+{
+putchar(x);
+putchar('\n');
+}
+return (0);
+}
