5060958ba0cd5ce38036181845ee049537b67a85
Bre R <2326@holbertonschool.com>
2020-09-21 06:48:56 +0000

    created new directory

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,20 +1,24 @@
+/**
+* main- void
+* description: print #'s 00 to 99
+* Return: 0
+*/
+
 #include <stdio.h>
 int main(void)
 {
-  int x = '0';
-  int y = '0';
+int x = '0';
+int y = '0';
 
-  if (x = 0 && y <= 9)
-    {
-      putchar(x);
-      putchar(y);
-      putchar(',');
-      putchar(' ');
-      putchar('\n');
-      y++;
-    }
-
-  else
-    x++;
-  return (0);
+for (x <= '9' ; x++)
+{
+for (y <= 9 ; y++)
+putchar(a);
+putchar(b);
+}
+putchar(',');
+putchar(' ');
+    
+}
+return (0);
 }
