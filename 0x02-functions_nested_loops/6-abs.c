84be7fb0141fed733ff37ef95c84bf6e24f1659a
Bre R <2326@holbertonschool.com>
2020-09-23 18:48:14 +0000

    commit previously modified files

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* int_abs- int
+*
+* description: computes absolute value of an int
+* Return: 0
+*/
+
+int _abs(int r)
+{
+if (r < 0)
+{
+return (-r);
+}
+else
+{
+return (r);
+}
+}
