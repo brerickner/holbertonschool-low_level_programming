6abf3057c49a87d3e37dbfe25d9f01ecf571c56b
Bre R <2326@holbertonschool.com>
2020-09-23 19:55:23 +0000

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+B#include<stdio.h>
 #include "holberton.h"
 
 /**
 * main- void
-* 
+*
 * description: computing sum of numbers
 * Return: 0
 */
@@ -12,14 +12,14 @@ int main(void)
 {
 int i, sum;
 
-sum=0;
-for(i=0;i<1024;++i)
+sum = 0;
+for (i = 0; i < 1024; ++i)
 {
-if((i%3==0)||(i%5==0))
+if ((i % 3 == 0) || (i % 5 == 0))
 {
-sum=sum+i;
+sum = sum + i;
 }
 }
-printf("%d",sum);
+printf("%d\n", sum);
 return (sum);
 }
