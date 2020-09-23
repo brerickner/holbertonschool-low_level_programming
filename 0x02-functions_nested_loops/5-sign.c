c71ce432cf4f582ff5428fadc7adf7c69c3f3f23
Bre R <2326@holbertonschool.com>
2020-09-23 01:18:26 +0000

    finished debugging

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+* print_sign- int n
+* @n: sign
+*
+* description: prints sign of number
+* Return: 1 if c is a L/letter, otherwise 0
+*/
+
+int print_sign(int n)
+{
+int r;
+
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
