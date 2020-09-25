b8d9c6dc4c778aa454f45e66d2224a640699fda6
Bre <2326@holbertonschool.com>
2020-09-25 03:25:12 +0000

    makin moves

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+* print_most_numbers - void
+* description: prints numbers 0 thru 9, excluding 2 and 4
+*
+* Return: void
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+	if (i != 50 && i != 52)
+	{
+	_putchar(i);
+	}
+	}	
+	_putchar('\n');
+}
