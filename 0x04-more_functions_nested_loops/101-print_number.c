d2bfc39adf21defeb7fcd866fdcde3ca213a9e02
Bre <2326@holbertonschool.com>
2020-09-25 04:25:52 +0000

    saving

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+* print_number - int n
+* @n: integer
+* description: print and integer
+* Return: void
+*/
+
+
+void print_number(int n)
+{
+
+if (n <= '0' &&  n >= '0')
+
+	_putchar(n);
+}
