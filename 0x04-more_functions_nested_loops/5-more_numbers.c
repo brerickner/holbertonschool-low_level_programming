cbc77b5997a55427d1a0bbb0cc9ae437fbf0701a
Bre <2326@holbertonschool.com>
2020-09-25 04:57:53 +0000

    last commit of night

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+* more_numbers - numbers on numbers
+* description: prints 10 times the numbers
+* Return: void
+*/
+
+void more_numbers(void)
+{
+  /* int i; */
+char k;
+
+for (k = 48; k <= 57; k++)
+{
+  /* for (i = 0; i <= 14; i++)*/
+_putchar(k);
+_putchar('\n');
+}
+}
