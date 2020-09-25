71c03dd0c7212233ec8c8b17e2ae506ea2c05abf
Bre <2326@holbertonschool.com>
2020-09-25 02:56:21 +0000

    saving goods

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* mul - int a and int b
+* @a: first integer
+* @b: second integer
+* description: finds product of two integers
+* Return: product value
+*/
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
