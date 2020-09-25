79cd578cbb46bf6c9b1f4360693d04e149014df7
Bre <2326@holbertonschool.com>
2020-09-25 05:55:10 +0000

    6-lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* print_line - int n
+* @n: number of times to print
+*
+* description: draws a straight line
+* Return: 0
+*/
+
+void print_line(int n)
+{
+n = 95;
+
+if (n <= 0)
+_putchar('\n');
+else
+_putchar(n);
+}
