84548ab2418b506e6cfebb869ce573e50f102222
Bre <2326@holbertonschool.com>
2020-09-26 00:08:40 +0000

    fixed code

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,41 +2,34 @@
 
 /**
 * main- void
-* description: Print fizz every 3rd multiple and Buzz for every 5th, 
-* else, FizzBuzz
+* description: Print fizz every 3rd multiple and Buzz for every 5th,
+* else FizzBuzz
 * Return: 0 if successful
 */
- 
+
 int main(void)
 {
 int i;
 
-for (i = 0; i <= 100; i++)
+for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0)
 {
 printf("Fizz");
 }
-else if (i % 5 == 0)
+if (i % 5 == 0)
 {
 printf("Buzz");
 }
-else if ((i % 3 && i % 5) == 0)
-{
-printf("FizzBuzz");
-}	
-else 
+if (i % 3 != 0 && i % 5 != 0)
 {
 printf("%d", i);
 }
-if (i < 100)
+if (i != 100)
 {
-putchar(' ');
+printf(" ");
 }
-else 
-{
-putchar(i);
-}  
-return 0;
 }
+putchar('\n');
+return (0);
 }
