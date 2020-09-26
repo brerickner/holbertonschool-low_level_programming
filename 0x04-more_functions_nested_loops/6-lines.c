d135c651e646c8921fd04298097e88326749558c
Bre <2326@holbertonschool.com>
2020-09-26 01:16:27 +0000

    6-lines.c

--- 0x04-more_functions_nested_loops/6-lines.c
+++ 0x04-more_functions_nested_loops/6-lines.c
@@ -4,6 +4,7 @@
 * print_line - print straight line
 * description: print straight line
 * @n: character
+* Return: void
 */
 
 void print_line(int n)
@@ -13,5 +14,8 @@ int line;
 if (n > 0)
 {
 for (line = 1; line <= n; line++)
+{
 _putchar('_');
+}
+_putchar('\n');
 }
