2b242ea95efdbd98c98b9b4468f31d7f01064558
Bre <2326@holbertonschool.com>
2020-09-29 06:10:31 +0000

    yessssss

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * puts2 - char *str
+ * @str: string to be printed
+ *
+ * description: function that prints every other character of string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int string;
+	char x = '\n';
+
+
+	for(string = 0; str[string] != '\0'; string += 2)
+	_putchar(str[string]);
+	_putchar(x);
+}
