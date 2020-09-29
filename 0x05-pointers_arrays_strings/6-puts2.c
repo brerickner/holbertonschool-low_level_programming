0880ac8163f0cb8ca9db8beeee679f48e04161dc
Bre <2326@holbertonschool.com>
2020-09-29 14:58:02 +0000

    changed code

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -14,7 +14,7 @@ void puts2(char *str)
 	char x = '\n';
 
 
-	for(string = 0; str[string] != '\0'; string++)
+	for (string = 0; str[string] != '\0'; string++)
 	if (string % 2 == 0)
 		_putchar(str[string]);
 	_putchar(x);
