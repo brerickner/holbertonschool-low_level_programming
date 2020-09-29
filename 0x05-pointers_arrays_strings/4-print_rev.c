00d609b3f0cdd76ee6dc93dbb367973f524703e9
Bre <2326@holbertonschool.com>
2020-09-29 04:24:59 +0000

    yayyyyyyyyyyy

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,12 +10,14 @@
 
 void print_rev(char *s)
 {
-	int string = 0;
+	int string;
 	char x = '\n';
 
+	string = 0;
 	while(s[string] != '\0')
+		string++;
 
-	for(string -= 1; s[string] >= 0 ; string++)
+	for(string -= 1; string >= 0 ; string--)
 	_putchar(s[string]);
 	_putchar(x);
 }
