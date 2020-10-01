d057c526cbdaaaf25434c58106f456c736514c8a
Bre <2326@holbertonschool.com>
2020-10-01 19:18:13 +0000

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,23 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
+ * *_strcat(char *dest, char *src)
+ * @dest: final string
+ * @src: string to be appeneded
+ * description: Append src string to dest string
+ * Return: *dest
  */
-
+char *_strcat(char *dest, char *src)
 {
+	int i, i2, both;
 
+	for(i = 0; dest[i] != '\0'; i++)
+		;
+	for(i2 = 1; src[i2] != '\0'; i2++)
+		;
+	both = i + i2;
+ 	*dest = both;
 
+	return (dest);
 
 }
