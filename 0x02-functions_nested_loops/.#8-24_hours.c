ea9c8fef89409a73b7743d9c45543c32d1c82a21
Bre <2326@holbertonschool.com>
2020-10-01 01:29:53 +0000

    pre vagrant halt

---
+++ 0x02-functions_nested_loops/.#8-24_hours.c
@@ -0,0 +1,1 @@
+vagrant@vagrant-ubuntu-trusty-64.7453:1600994856
--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,12 +6,15 @@
 /**
 * main - void
 *
-* description: generates random password for 101-crackme
+* description: generates random valid password for 101-crackme
 * Return: randomNumber
 */
 
 int main(void)
 {
+
+	/* things to try: array to dictate how long the valid password array is */
+
 	srand(time(NULL));
 	int randomNumber = rand();
 
