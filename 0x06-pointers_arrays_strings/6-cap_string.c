bb27b91b6b23770c7d0fefd0c549af9c9b1aa3a4
Bre <2326@holbertonschool.com>
2020-10-02 03:54:41 +0000

    getting credit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * *cap_string - char *s
+ * @s: string
+ *
+ * description: capitalizes all words of a string
+ * Return: s
+ */
+char *cap_string(char *s)
+{
+	int i, delt = 'a' - 'A';
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= delt;
+		}
+		return (s);
+	}
+}
