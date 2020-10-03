6c4523d8b3a530e317a49f9dcb2051807c9a715a
Bre <2326@holbertonschool.com>
2020-10-03 01:39:53 +0000

    lets try arrays instead of strings

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -9,12 +9,12 @@
 char *rot13(char *s)
 {
 	int i, i2;
-	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char *rot_13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxwzabcdefghijklm";
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot_13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxwzabcdefghijklm";
 
 	for (i = 0; s[i]; i++)
 	{
-		for (i2 = 0; i2 < 52; i2++)
+		for (i2 = 0; i2 < ((alpha[i] + rot_13[i]) - 12); i2++)
 		{
 			if (alpha[i2] == s[i])
 				s[i] = rot_13[i2];
