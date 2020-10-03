e10c7d34f2016f4e8e3b080ba6157c63cee06beb
Bre <2326@holbertonschool.com>
2020-10-03 02:50:41 +0000

    Not sure yet

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,11 +9,21 @@
  */
 char *cap_string(char *s)
 {
-	int i, delt = 'a' - 'A';
+	int i, i2;
+	int delt = 'a' - 'A';
+	char spesh[] = "\t\n;,!\"() {}.?";
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; spesh[i]; ++i)
 	{
-		s[i] -= delt;
-		return (s);
+		for (i2 = 0; spesh[i2] < s[i]; i2++)
+		{
+			if (s[i] == spesh[i])
+			{
+				i++;
+				s[i] -= delt;
+				s[i] = spesh[i];
+			}
+		}
 	}
+	return (s);
 }
