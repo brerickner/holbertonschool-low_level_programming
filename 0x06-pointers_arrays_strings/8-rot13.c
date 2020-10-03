317a7bc89426d060812441600afc9c662cd7dadb
Bre <2326@holbertonschool.com>
2020-10-03 01:17:05 +0000

    outputting good i think

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -4,23 +4,22 @@
  * *rot13 - char *s
  * @s: string being encoded
  * description: function that encodes a string using rot13
- * Return: answer
+ * Return: s
  */
 char *rot13(char *s)
 {
-	int i;
-	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdevghijklmnoqrstuvwxyz";
-	char *rot_13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrituvwxyzabdefghijklm";
-	char *answer = s;
+	int i, i2;
+	char *alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *rot_13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxwzabcdefghijklm";
 
-	while (*s)
+	for (i = 0; s[i]; i++)
 	{
-		for (i = 0; alpha[i]; i++)
+		for (i2 = 0; i2 < ((alpha[i] + rot_13[i]) - 12); i2++)
 		{
-			if (*s == alpha[i])
-				*s = rot_13[i];
+			if (alpha[i2] == s[i])
+				s[i]= rot_13[i2];
+			break;
 		}
-		s++;
 	}
-	return (answer);
+	return (s);
 }
