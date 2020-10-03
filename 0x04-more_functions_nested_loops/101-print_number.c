71aec8e5cc833c56270290c2cfa8521fafe1ce5e
Bre <2326@holbertonschool.com>
2020-10-03 02:55:23 +0000

    finished this one earlier

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,17 +1,40 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
-* print_number - int n
-* @n: integer
-* description: print and integer
-* Return: void
-*/
-
+ * print_number - int n
+ * @n: the num
+ * description: print an integer
+ * Return: void
+ */
 
 void print_number(int n)
 {
+        char a;
+        int b;
+        char c;
 
-if (n <= '0' &&  n >= '0')
-
-	_putchar(n);
+        if (n < 0)
+        {
+                _putchar('-');
+                a = ('0' - (n % 10));
+                n /= -10;
+        }
+        else
+        {
+                a = ((n % 10) + '0');
+                n /= 10;
+        }
+        b = 0;
+        while (n > 0)
+        {
+                b = b * 10 + (n % 10);
+                n /= 10;
+        }
+        while (b > 0)
+        {
+                c = ((b % 10) + '0');
+                _putchar(c);
+                b /= 10;
+        }
+        _putchar(a);
 }
