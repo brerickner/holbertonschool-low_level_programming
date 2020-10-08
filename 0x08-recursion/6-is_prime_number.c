820a9aa54b1b84cf90f81537e3564d18006063fe
Bre <2326@holbertonschool.com>
2020-10-08 04:31:44 +0000

    reaching for some checks

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - function
+ * description: function to determine if a number is prime
+ * @n: number to test
+ * Return: prime or not
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+}
+
+/**
+ * the_prime - my function
+ * description: prime number test
+ * @n: number to pass
+ * @x: variable
+ * Return: 1, 0
+ */
+int the_prime(int n, int x)
+{
+	if (x == 1)
+		return (1);
+
+}
