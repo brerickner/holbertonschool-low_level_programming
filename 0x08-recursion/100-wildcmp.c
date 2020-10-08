eb805a3d88bc85d8faa48d7342f610be0f713298
Bre <2326@holbertonschool.com>
2020-10-08 04:54:58 +0000

    saving

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,50 +1,17 @@
 #include "holberton.h"
 
 /**
- * _strlen_recursion - char *s
- * @s: string that's being measured for it's length
- * Description: function that measures the length of a string, recursively
- * Return: length
- */
-int _strlen_recursion(char *s)
-{
-	int length = 0;
-
-	if (*s)
-	{
-		length++;
-		length += _strlen_recursion((s) + 1);
-	}
-	return (length);
-}
-
-/**
- * _print_rev_recursion - char *s
- * @s: string being printed
- * Description: function that prints a string in reverse
- * Return: void
- */
-void _print_rev_recursion(char *s)
-{
-	if (*s) /* if pointing to char in string */
-	{
-		_print_rev_recursion((s) + 1); /* blanket string w/ pointers */
-		_putchar(*s); /* start printing from most recent position */
-		/* pointed to in stack(words?) */
-	}
-}
-/**
- * is_palindrome - char *s
- * description: function to test if string is a palindrome
- * @s: the string we're testing if palindrome
- * Return: 1 if it is a palindrom. else 0
+ * wildcmp - char *s1, char *s2
+ * description: function that compares two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 1 if it does stuff, 0
  */
 
-int is_palindrome(char *s)
+int wildcmp(char *s1, char *s2)
 {
-	if (*s == *s)
-	{
+	if ((*s1 == '\0') && (*s2 == '\0'))
 		return (1);
-	}
-	return (0);
+	else
+		return (0);
 }
