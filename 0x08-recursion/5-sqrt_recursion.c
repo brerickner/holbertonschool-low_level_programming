4dca742437b0efe08b809d753fb954cd1c67dd4c
Bre <2326@holbertonschool.com>
2020-10-08 04:15:05 +0000

    saving just in case

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/* FUNCTIONS USED */
+
+ /**
+ * _sqrt_recursion - int n
+ * @n: natural number
+ * description: return natural sq root of a #
+ * Return: squirt
+ */
+
+int _sqrt_recursion(int n)
+{
+	int squirt = 0;
+
+	return (capture_squirt(n, squirt));
+}
+
+/**
+ * capture_squirt - int nature, int squirt
+ * @n: or nature, as in, what we're trying to make happen
+ * @squirt: what we must capture to make nature happen
+ * description: this is actually a function to find a natural #'s square root
+ * but disquised in a way that I can understand
+ * Return: squirt if captured successfully. -1 if squirts don't make it happen
+ */
+
+int capture_squirt(int n, int squirt)
+{
+
+	if ((squirt * squirt) == n)
+		return (squirt);
+/* if squirt & squirt are too much for nature, it can't happen. return error*/
+	if ((squirt * squirt) > n)
+		return (-1);
+
+	else
+		return (squirt(n, squirt + 1));
+}
