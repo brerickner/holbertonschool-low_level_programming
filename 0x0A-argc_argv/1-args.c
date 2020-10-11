3ca25b98f9c21955465b20b61b50de42147dd099
Bre <2326@holbertonschool.com>
2020-10-11 04:03:15 +0000

    I tried subtracting 1 from argc being printed

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * main - int argc, char *argv[]
  * @argc: argument count
  * @argv: argument vector
- * description: program that prints its name
+ * description: program that prints argument count
  * Return: 0 if successful
  */
 int main(int argc, char *argv[])
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	if (argc >= 0)
 
-		printf("%d\n", argc);
+		printf("%d\n", argc - 1);
 
 	return (0);
 }
