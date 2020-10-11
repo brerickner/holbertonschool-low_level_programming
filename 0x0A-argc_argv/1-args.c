02605748ed393c015ff1063a90e6497f94b9694f
Bre <2326@holbertonschool.com>
2020-10-11 03:42:28 +0000

    back to 2 checks

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,17 +6,21 @@
  * main - int argc, char *argv[]
  * @argc: argument count
  * @argv: argument vector
- * description: program that prints number of arguments
+ * description: program that prints its name
  * Return: 0 if successful
  */
 int main(int argc, char *argv[])
 {
-	int index = 0;
-	(void)argv;
+	int index, sum = 0;
 
-	for (index = 0; index < argc; index++)
-	{}
-	printf("%d\n", index);
+	if (argc >= 0)
+	{
 
+		for (index = 0; index < argc; index++)
+		{
+			sum += atoi(argv[index]);
+		}
+		printf("%d\n", sum);
+	}
 	return (0);
 }
