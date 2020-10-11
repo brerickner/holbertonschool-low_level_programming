2d0309427a5a47c928390fe041dd82bd2a8439c0
Bre <2326@holbertonschool.com>
2020-10-11 18:59:05 +0000

    added minimum code

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * _atoi - char *s
+ * s@: string to be converted to integer
+ * description: function that converts a string to an integer
+ * Return: 0 if successful
+ */
+int _atoi(char *s)
+{
+	return (0);
+}
