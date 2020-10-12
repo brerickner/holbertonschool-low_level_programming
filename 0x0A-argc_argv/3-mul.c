a7f1401c661e813b7e8315a737abf7033ca2fd56
Bre <2326@holbertonschool.com>
2020-10-12 02:08:56 +0000

    adding minimum

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - int argc, char **argv
+ * @argc: argument count
+ * @argv: argument vector
+ * description: Program that multiplies two numbers
+ * Return: 1 if two arguments are not received. Else 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	return (0);
+}
