2fa76fbab26bf21a9667399399d3c16bf4d4abfa
Bre <2326@holbertonschool.com>
2020-10-12 04:19:42 +0000

    i don't really know what to do

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - int argc, char **argv
@@ -8,10 +9,26 @@
  * description: Program that adds positive #'s
  * Return: 1 if symbol is not a digit. Else 0.
  */
+
 int main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	int index, result = 0;
 
+	for (index = 1; index < argc; index++)
+	{
+		if (atoi(*argv) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result += atoi(argv[index]);
+	}
+
+	if (argc == 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	printf("%d\n", result);
 	return (0);
 }
