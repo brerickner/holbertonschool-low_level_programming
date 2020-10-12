1ba85e4ddc35fce4c7767cebad6d984200efeb38
Bre <2326@holbertonschool.com>
2020-10-12 03:08:36 +0000

    saving progress

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,10 +8,25 @@
  * description: Program that multiplies two numbers
  * Return: 1 if two arguments are not received. Else 0.
  */
+
 int main(int argc, char **argv)
 {
-	(void)argc;
+	int index, result = 1;
 	(void)argv;
 
+	for (index = 1; index < argc; index++)
+	{
+		if (argc < 3)
+		{
+			printf("Error\n");
+
+			return (1);
+		}
+		else
+		{
+			result *= index;
+			printf("%d\n", result);
+		}
+	}
 	return (0);
 }
