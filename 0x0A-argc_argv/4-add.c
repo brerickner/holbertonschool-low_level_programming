2e41fa6936b7d7c7962c24e1744a9e830ca064cd
Bre <2326@holbertonschool.com>
2020-10-12 02:11:46 +0000

    minimum code

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - int argc, char **argv
+ * @argc: argument count
+ * @argv: argument vector
+ * description: Program that adds positive #'s
+ * Return: 1 if symbol is not a digit. Else 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	return (0);
+}
