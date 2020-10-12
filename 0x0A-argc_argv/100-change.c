4ab09359552cca43743d5008ba8623558050f285
Bre <2326@holbertonschool.com>
2020-10-12 02:36:58 +0000

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - int argc, char **argv
+ * @argc: argument count
+ * @argv: argument vector
+ * description: Program that prints the min # of coins needed to make change
+ * Return: 1 if # of arguments passed is not 1. Else 0.
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+
+	return (0);
+}
