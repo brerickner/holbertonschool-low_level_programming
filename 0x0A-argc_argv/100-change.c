ffca8b6d359ec28f3d01f6020e9bd44dee4d5352
Bre <2326@holbertonschool.com>
2020-10-13 04:18:10 +0000

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - int argc, char **argv
@@ -8,10 +9,47 @@
  * description: Program that prints the min # of coins needed to make change
  * Return: 1 if # of arguments passed is not 1. Else 0.
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
+	int coins = 0;
+	int change = atoi(argv[1]);
 
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (change < 0)
+	{
+		printf("0\n");
+	}
+	while (argc == 2)
+	{
+		if (change - 25 >= 0)
+		{
+			change -= 25;
+			coins++;
+		}
+		if (change - 10 >= 0)
+		{
+			change -= 0;
+			coins++;
+		}
+		if (change - 5 >= 0)
+		{
+			change -= 5;
+			coins++;
+		}
+		if (change - 2 >= 0)
+		{
+			change -= 2;
+			coins += 2;
+		}
+		else
+		{
+			coins++;
+		}
+	}
+	printf("%d\n", coins);
 	return (0);
 }
