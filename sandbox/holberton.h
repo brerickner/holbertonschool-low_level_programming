429738732484681b864f5364bd86502a2ecd0972
Bre <2326@holbertonschool.com>
2020-10-13 19:53:33 +0000

    whats going on

---
+++ sandbox/holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+int the_prime(int n, int x);
+int capture_squirt(int nature, int squirt);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif /* _HOLBERTON_H_*/
