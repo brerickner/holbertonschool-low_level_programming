73ac5141fd90894b3b97382f54a41a7e0584bd1c
Bre <2326@holbertonschool.com>
2020-10-15 19:17:20 +0000

    saving comments

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *malloc_checked - unsigned int b
+ * @b: amount of memory to allocate
+ * description: funtion that allocates memory using malloc
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	_putchar('.');
+}
