cd78133fd522ae85b4d2de74344eb4258532cd6a
Bre <2326@holbertonschool.com>
2020-10-15 19:23:41 +0000

    saving comments

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *string_noncat - char *s1, char *s2, unsigned int n
+ * @s1: pointer to memory that was allocated
+ * @s2: string being appended to S1
+ * @n: number of bytes to add from s2
+ * description: function that allocates memory for an array, using malloc
+ * Return: S1 with n bytes of S2 appended plus null terminated. Else NULL if
+ * function fails
+ */
+char *string_noncat(char *s1, char *s2, unsigned int n)
+{
+	return (s1);
+}
