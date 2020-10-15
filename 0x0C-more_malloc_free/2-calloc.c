da049441d384d68382a48b0d2f6eff276aff5913
Bre <2326@holbertonschool.com>
2020-10-15 19:27:34 +0000

    saving comments

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_calloc - unsigned int nmemb, unsigned int size
+ * @nmemb: # of elements to read
+ * @size: size of each element in byte
+ * description: funtion that allocates memeory for an array, using malloc
+ * Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	(void)nmemb;
+	(void)size;
+
+	_putchar('\n');
+}
