437075a972cf0c7afc8e2051d824084617f73f4c
Bre <2326@holbertonschool.com>
2020-10-16 04:18:46 +0000

    woah, I kinda nailed that!

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -11,8 +11,20 @@
  */
 int *array_range(int min, int max)
 {
-	(void)min;
-	(void)max;
+	int *array, index, size;
 
-	return (NULL);
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+
+	array = malloc(sizeof(int) * size);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = min++;
+
+	return (array);
 }
