c4e5ab55c880ab852c73e9d8f90fa13dc5a7124c
Bre <2326@holbertonschool.com>
2020-10-16 02:19:08 +0000

    min checks

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * main - int argc, char *argv[]
+ * @argc: argument count
+ * @argv: argument vector
+ * description: program that mutiplies two unsigned integers
+ * Return: 0 if successful.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+
+	exit(98);
+	return (0);
+}
