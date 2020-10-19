c774518b639ad18f0a6d2c1b1f37936e2dd973e4
Bre <2326@holbertonschool.com>
2020-10-19 21:50:36 +0000

    my name is AWAAVIAUIATL ??

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+/**
+ * init_dog - struct dog *d, char *name, float age, char *owner
+ * @d: variable of struct dog type
+ * @name: pointer to member name of type char
+ * @age: member age of type float
+ * @owner: pointer to member owner of type char
+ * description: function that initializes variable of type struct dog
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog doggy;
+
+	d = &doggy;
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
