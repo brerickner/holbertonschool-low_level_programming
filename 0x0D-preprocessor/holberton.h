30a8e8e45d34057909875509bceda7db93dca420
Bre <2326@holbertonschool.com>
2020-10-19 15:30:39 +0000

    creating files, header, readme

---
+++ 0x0D-preprocessor/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+char *_strncat(char *dest, char *src, unsigned int n);
+unsigned int _strlen(char *s);
+int _putchar(char c);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int *array_range(int min, int max);
+void *_calloc(unsigned int nmemb, unsigned int size);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+
+
+#endif /* _HOLBERTON_H_*/
