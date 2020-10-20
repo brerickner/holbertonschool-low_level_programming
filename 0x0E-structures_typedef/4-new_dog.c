18de460c4a82e9543a3463f143fd19ea88184875
Bre <2326@holbertonschool.com>
2020-10-20 02:11:26 +0000

    saving so far

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * dog_t *new_dog - char *name, float age, char *owner
+ * @name: member pointer to type char
+ * @age: member of type float
+ * @owner: member pointer to type char
+ * description: funtion that creates a new dog
+ * Return: NULL upon failure. else 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (new_dog == NULL)
+		return (NULL);
+}
