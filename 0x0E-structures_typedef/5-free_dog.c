b16caa13a4890e4ca341b54f933c2a5fae24aebe
Bre <2326@holbertonschool.com>
2020-10-20 02:22:04 +0000

    saving progress

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * void free_dog(dog_t *d);
+ * @d:
+ * description: function that frees dogs
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	(void)d;
+
+	return (NULL);
+}
