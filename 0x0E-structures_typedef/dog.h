de09481575464a3a2835dcea815f5632809be63d
Bre <2326@holbertonschool.com>
2020-10-20 01:51:20 +0000

    added typedef to change struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,13 +9,17 @@
  *
  * Description: defining new type struct dog, with name, age and owner elements
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
