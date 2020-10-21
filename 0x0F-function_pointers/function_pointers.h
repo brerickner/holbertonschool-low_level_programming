37070621ec6e34f840b89cc9e51b16381acd9e27
Bre <2326@holbertonschool.com>
2020-10-21 17:51:11 +0000

    commenting out future prototypes

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,8 +2,9 @@
 #define FUNCTION_POINTERS_H
 
 int _putchar(char c);
-void array_iterator(int *array, size_t size, void (*action)(int));
 void print_name(char *name, void (*f)(char *));
-int int_index(int *array, int size, int (*cmp)(int));
+
+/* void array_iterator(int *array, size_t size, void (*action)(int));
+   int int_index(int *array, int size, int (*cmp)(int)); */
 
 #endif /*__FUNCTION_POINTERS_H__*/
