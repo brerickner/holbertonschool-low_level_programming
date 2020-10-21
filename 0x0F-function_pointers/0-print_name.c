f0e0bd13b4534e7fe312bd3bd9c69782c3ac222e
Bre <2326@holbertonschool.com>
2020-10-21 22:32:10 +0000

    messed a lot of things up trying to define null some other way, returning to what works with checker for task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,6 @@
 #include "function_pointers.h"
 #define NULL 0
+/*#define NULL 0 tried defining in function_pointers.h instead*/
 /**
  * print_name -char *name, void (*f)(char *)
  * @name: pointer to type char
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,13 +1,13 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+
+/* #include <stdlib.h> used for NULL and size_t data type*/
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-struct size_t size
-{
-	int elem;
-};
+/*void array_iterator(int *array, size_t size, void (*action)(int));*/
+
 /*   int int_index(int *array, int size, int (*cmp)(int)); */
 
 #endif /*__FUNCTION_POINTERS_H__*/
