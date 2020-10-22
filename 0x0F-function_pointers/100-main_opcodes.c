b49afc7fefd96ae264a93f474df6ed0c1ac258d8
Bre <2326@holbertonschool.com>
2020-10-22 04:58:11 +0000

    min code

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - void
+ * description: function that searches for an integer
+ * Return: program that prints opcodes of own main function
+ */
+int main (void)
+{
+
+	return(0);
+}
