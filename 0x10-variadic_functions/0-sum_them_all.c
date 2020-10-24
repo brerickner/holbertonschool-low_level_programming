5c4055536ab5606354a001a536d5ab98b692b208
brerickner <breannarickner@gmail.com>
2020-10-24 18:46:54 +0000

    betty compliant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-/*
- * sum_them_all- const unsigned int n, ...
+/**
+ * sum_them_all - const unsigned int n, ...
  * @n: number of
  * description: function that adds arguments
- * return: sum if not 0
+ * Return: sum if not 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned index = 0, sum = 0;
+	unsigned int index = 0, sum = 0;
 	va_list sum_list;
 
 	if (n == 0)
