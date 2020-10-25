88f55185d22d96ecd6f4e0221e878925f8d5a339
brerickner <breannarickner@gmail.com>
2020-10-25 23:47:15 +0000

    saving

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,28 +1,21 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
+#include <stdarg.h>
+#include <stdlib.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void char_func(args_list);
-void string_func(args_list);
-void int_func(args_list);
-void string_func(args_list);
+void char_func(va_list args_list);
+void string_func(va_list args_list);
+void int_func(va_list args_list);
+void string_func(va_list args_list);
 
-/**
- * struct print_stuff - struct type that takes char and *eat() as args
- */
-struct print_stuff[] = {
-{'c', char_func}
-{'f', float_func}
-{'i', int_func}
-{'s', string_func}
-{NULL, NULL}
-}; int outer = 0; /* take out 0 if messing up */
-
-typedef struct print_stuff{
-	char type_c;
+typedef struct c_type
+{
+	char c;
 	void(*fun)();
-}
+} print_stuff;
+
 #endif /* VARIADIC_FUNCTION_H */
