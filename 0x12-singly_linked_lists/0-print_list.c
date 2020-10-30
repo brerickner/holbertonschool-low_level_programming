453dc5ab742e18870e35de2c205c427adec6d0c9
brerickner <breannarickner@gmail.com>
2020-10-30 23:02:50 +0000

    switchin to a for loop, no legit reason

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,16 +13,12 @@ size_t print_list(const list_t *h)
 {
 	size_t nodes;
 
-	while (h != NULL)
+	for (nodes = 0; h != NULL; h = h->next, nodes++)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
 			printf("[%u] %s\n", h->len, h->str);
-		nodes++;
-		h = h->next;
 	}
 	return (nodes);
 }
