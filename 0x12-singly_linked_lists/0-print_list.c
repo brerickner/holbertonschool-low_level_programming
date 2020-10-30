34e8ea483bdd8ff2310d52ad4cc93cf1829136c3
brerickner <breannarickner@gmail.com>
2020-10-30 20:47:17 +0000

    not outputting correctly but finally compiling, so saving

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * size_t print_list - const list_t *h
+ * @h: pointer to list_s node
+ * description: function that prints all elemets of a list_t list.
+ * Return: # of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int nodes;
+
+	if (h->str == NULL)
+		printf("[%i] %s", 0, "(nil)");
+	else
+		nodes = printf("[%u]%s\n",h->len, h->str);
+	return (nodes);
+}
