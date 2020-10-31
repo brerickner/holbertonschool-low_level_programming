6033b5410cc653e9c067fa128d6918e8a007a5df
brerickner <breannarickner@gmail.com>
2020-10-31 03:15:32 +0000

    betty

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_list - list_t *head
+ * @head: pointer to list
+ * description: function that frees malloc
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	(void)head;
+}
