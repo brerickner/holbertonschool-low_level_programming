a7ad15c470511007d6b68b12d6c81621b930aaaa
brerickner <breannarickner@gmail.com>
2020-10-31 04:23:27 +0000

    updating header prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -25,7 +25,7 @@ size_t print_list(const list_t *h);
 int _putchar(char c);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-int _strlen(char *s);
+int _strlen(const char *s);
 list_t *create_node(list_t *new_node);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
