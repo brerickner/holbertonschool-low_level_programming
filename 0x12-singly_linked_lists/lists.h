7e71598e2b0ecff1444c8f17a6623dad5ab93574
brerickner <breannarickner@gmail.com>
2020-10-31 02:51:08 +0000

    saving header

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -24,5 +24,7 @@ size_t print_list(const list_t *h);
 int _putchar(char c);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+int _strlen(char *s);
+list_t *create_node(list_t *new_node);
 
 #endif /* LISTS_H */
