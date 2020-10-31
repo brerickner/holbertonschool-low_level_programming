c8f5c2fe6f100dbbb7c87af2a5febd3e8913303f
brerickner <breannarickner@gmail.com>
2020-10-31 03:52:01 +0000

    installed nasm and attempted to plug info in to format found on line

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,21 @@
+;; 100-hello_holberton.asm
+;; description: print a string using printf
+
+extern	printf	; the C function, to be called
+section .data	; Data section, initialized variables
+msg: db "Hello Holberton", 0 ; C string needs 0
+fmt: db "%s", 10, 0          ; The printf format, "\n",'0'
+
+section .text	; Code section.
+global main	; the standard gcc entry point
+main:				; the program label for the entry point
+push    rbp
+
+mov	rdi,fmt
+mov	rsi,msg
+mov	rax,0		; or can be  xor  rax,rax
+call    printf	; Call C function
+
+pop	rbp		; restore stack
+mov	rax,0		; normal, no error, return value
+ret			; return
