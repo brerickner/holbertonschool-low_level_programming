2934220b7d6c4f9e7482f085264e60ae7689c4a1
brerickner <breannarickner@gmail.com>
2020-11-03 16:28:33 +0000

    betty compliant comments

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -4,7 +4,7 @@
 /**
  * listint_t *get_nodeint_at_index - listint_t *head, unsigned int index
  * @head: pointer to node in listint_h
- * index: index of the node, starting at 0
+ * @index: index of the node, starting at 0
  * description: function that returns the nth node of a listint_t linked list
  * Return: nth node of listint_t linked list. Else NULL if node does not exist
  */
@@ -13,5 +13,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	(void)head;
 	(void)index;
 
-	return(0);
+	return (0);
 }
