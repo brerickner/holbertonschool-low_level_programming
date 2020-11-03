5efa7fd0241dc4e9fb4238aeb641d15f203ee0e2
brerickner <breannarickner@gmail.com>
2020-11-03 15:52:03 +0000

    comments for 6-pop

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * int pop_listint - listint_t **head
+ * @head: pointer to node in listint_h
+ * description: function that deletes the head node of lintint_t and returns
+ * the head nodes data(n)
+ * Return: 0 if linked list empty. Else data of head node.
+ */
+int pop_listint(listint_t **head)
+{
+	(void)head;
+
+	return(0);
+}
