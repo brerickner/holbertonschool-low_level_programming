dc1671b9b2c76139e6902c8bcc339f4d156588b9
brerickner <breannarickner@gmail.com>
2020-11-03 15:54:15 +0000

    edited comments

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,14 +3,13 @@
 
 /**
  * void free_listint - listint_t *head
- * @h: pointer to node in listint_h
+ * @head: pointer to node in listint_h
  * description: function that frees a listint_t list
  * Return: void
  */
 void free_listint(listint_t *head)
 {
 	(void)head;
-	(void)n;
 
 	return(0);
 }
