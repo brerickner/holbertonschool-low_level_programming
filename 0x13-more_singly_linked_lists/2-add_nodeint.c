6b73db559b459e51f010b646bd2e770a5632da3d
brerickner <breannarickner@gmail.com>
2020-11-03 15:20:11 +0000

    adding comments

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_t *add_nodeint - listint_t **head, const int n
+ * @h: pointer to node in listint_h
+ * definition: function that adds a new node at beginning of listint_t
+ * Return: address of new element. Else NULL if failed.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	(void)head;
+	(void)n;
+
+	return(0);
+}
