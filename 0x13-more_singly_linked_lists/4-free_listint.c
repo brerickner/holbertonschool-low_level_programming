bdf8cc1e58bb757863e980fdcd97cc71b6b28a83
brerickner <breannarickner@gmail.com>
2020-11-03 15:27:28 +0000

    commmmmmmments :)

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * void free_listint - listint_t *head
+ * @h: pointer to node in listint_h
+ * definition: function that frees a listint_t list
+ * Return: void
+ */
+void free_listint(listint_t *head)
+{
+	(void)head;
+	(void)n;
+
+	return(0);
+}
