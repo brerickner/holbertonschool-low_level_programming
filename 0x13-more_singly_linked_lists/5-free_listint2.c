b2c323d8546e4c02b19d0a92bf681cd10dfb1b79
brerickner <breannarickner@gmail.com>
2020-11-03 15:34:45 +0000

    just realized I had definition instead of description, so I have to go back and redo all my comments now

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * void free_listint2 - listint_t **head
+ * @h: pointer to node in listint_h
+ * description: function that frees a listint_t list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	(void)head;
+
+	return(0);
+}
