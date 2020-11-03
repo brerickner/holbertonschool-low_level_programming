fd38d4de4259ad70c0192a21a490a02448c7a342
brerickner <breannarickner@gmail.com>
2020-11-03 16:36:04 +0000

    3 betty compliant

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 /**
- * listint_t *add_nodeint_end - listint_t **head, const int n
- * @h: pointer to node in listint_h
+ * *add_nodeint_end - listint_t **head, const int n
+ * @head: pointer to node in listint_h
+ * @n: integer in node
  * description: function that adds a new node at the end of  listint_t
  * Return: address of new element. Else NULL if failed.
  */
@@ -12,5 +13,5 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	(void)head;
 	(void)n;
 
-	return(0);
+	return (0);
 }
