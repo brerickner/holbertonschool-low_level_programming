cf50e20450986bd704682d1722325dd2f4526647
brerickner <breannarickner@gmail.com>
2020-11-03 16:09:44 +0000

    adding comments

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * int sum_listint - listint_t *head
+ * @head: pointer to node in listint_h
+ * description: function that returns summ of all the data (n) of listint_t
+ * Return: 0 if list is empty. Else sum of all the data (n)
+ */
+int sum_listint(listint_t *head)
+{
+	(void)head;
+
+	return(0);
+}
