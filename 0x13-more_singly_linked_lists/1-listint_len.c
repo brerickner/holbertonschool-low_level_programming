b919e484d0ac762c63e5a5aa9731afab4bde1e10
brerickner <breannarickner@gmail.com>
2020-11-03 15:15:51 +0000

    knocking out some comments

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * size_t listint_len - const listint_t *h
+ * @h: pointer to node in listint_h
+ * definition: prints all elements of a listint_t
+ * Return: # of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	(void)h;
+
+	return(0);
+}
