663b05fef80aed3058f6223deb8aede1238ca26b
brerickner <breannarickner@gmail.com>
2020-11-03 16:38:57 +0000

    betty compliant

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * size_t listint_len - const listint_t *h
+ * listint_len - const listint_t *h
  * @h: pointer to node in listint_h
  * definition: prints all elements of a listint_t
  * Return: # of nodes
@@ -11,5 +11,5 @@ size_t listint_len(const listint_t *h)
 {
 	(void)h;
 
-	return(0);
+	return (0);
 }
