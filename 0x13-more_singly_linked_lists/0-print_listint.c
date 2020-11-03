ce400b862fd59a030313323e58e10f3940d6fa71
brerickner <breannarickner@gmail.com>
2020-11-03 15:14:01 +0000

    writing comments out before dentist appointment

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * size_t print_listint - const listint_t *h
+ * @h: pointer to node in listint_h
+ * definition: prints all elements of a listint_t
+ * Return: # of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	(void)h;
+
+	return(0);
+}
