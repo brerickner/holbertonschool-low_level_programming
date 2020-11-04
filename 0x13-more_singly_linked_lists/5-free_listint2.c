a491cc237af6a495370f304639285064cf7c854f
brerickner <breannarickner@gmail.com>
2020-11-04 16:50:07 +0000

    no memory leaks, but it is outputting a (nil) at the end.... saving progress

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,13 +3,18 @@
 
 /**
  * free_listint2 - listint_t **head
- * @head: pointer to node in listint_h
+ * @head: pointer to head node address in listint_h
  * description: function that frees a listint_t list
  * Return: void
  */
 void free_listint2(listint_t **head)
 {
-	(void)head;
+	listint_t *buffer;
 
-	return (0);
+	for (buffer = *head; buffer != NULL; buffer = *head)
+	{
+		buffer = (*head)->next;
+		free(*head);
+		*head = buffer;
+	}
 }
