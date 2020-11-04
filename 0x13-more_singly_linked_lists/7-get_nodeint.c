f8d9002da0fec18698be251328260e70c891037c
brerickner <breannarickner@gmail.com>
2020-11-04 21:34:47 +0000

    not compiling yet but saving progress and uncommented out task 7 prototype from header

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,15 +3,22 @@
 
 /**
  * listint_t *get_nodeint_at_index - listint_t *head, unsigned int index
- * @head: pointer to node in listint_h
- * @index: index of the node, starting at 0
+ * @head: pointer to head node in listint_h
+ * @index: index of nodes in listint_t, starting at 0
  * description: function that returns the nth node of a listint_t linked list
- * Return: nth node of listint_t linked list. Else NULL if node does not exist
+ * Return: nth node of linked list. Else NULL if node does not exist
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	(void)head;
-	(void)index;
-
-	return (0);
+	if (!head)
+		return (NULL);
+	else
+	{
+		if (!head[index])
+			return (NULL);
+		for (index = 0; head[index]; index++)
+		{
+			if (head[index] == head->n)
+				return (head[index]);
+	}
 }
