ea97ae517f147dc128d2ff022586ea94078ed04c
brerickner <breannarickner@gmail.com>
2020-11-04 23:55:49 +0000

    cant lose this train of thought gotta save

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,13 +3,20 @@
 
 /**
  * sum_listint - listint_t *head
- * @head: pointer to node in listint_h
- * description: function that returns summ of all the data (n) of listint_t
- * Return: 0 if list is empty. Else sum of all the data (n)
+ * @head: pointer to head node in listint_h
+ * description: function that returns sum of all the data(n) of listint_t
+ * Return: 0 if list is empty. Else sum of all the data(n)
  */
 int sum_listint(listint_t *head)
 {
-	(void)head;
+	int sum;
 
-	return (0);
+	if (head == NULL)
+		return (0);
+	else
+	for (sum = 0; head; head = head->next; sum++;)
+		sum += sum;
+
+	return (sum);
+
 }
