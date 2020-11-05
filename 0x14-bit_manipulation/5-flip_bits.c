c934afc388e634b8a2f14f2333fad3bbba8c17bf
brerickner <breannarickner@gmail.com>
2020-11-05 23:57:55 +0000

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * flip_bits - unsigned long int n, unsigned long int m
+ * @n: first number being flipped
+ * @m: second number being flipped
+ * description: function that returns the # of bits needing switched to get
+ * from # n to # m
+ * Return: # of bits it takes to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int bitFlips;
+
+	return (bitFlips);
+}
