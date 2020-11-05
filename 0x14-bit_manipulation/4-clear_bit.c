99ac821bc23996aa76818a476f191def1da77fd6
brerickner <breannarickner@gmail.com>
2020-11-05 23:42:42 +0000

    adding betty compliant comments

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * clear_bit - unsigned long int *n, unsigned int index
+ * @n: number to manipulate bit in
+ * @index: index to find bit to manipulate
+ * description: function that sets the value of bit to 0 at index
+ * Return: 1 if it worked. Else -1 if error
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	return (-1);
+}
