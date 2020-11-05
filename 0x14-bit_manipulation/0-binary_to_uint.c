a422b68ed5f99754b5e653fb8d4006a0346b6c4c
brerickner <breannarickner@gmail.com>
2020-11-05 23:15:46 +0000

    saving comments

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * int binary_to_uint - const char *b
+ * @b: string of 0 and 1 chars
+ * description: function that converts a binary to unsigned int
+ * Return: the converted number. Else 0 if chars aren't 1 or 0 or b is NULL
+ */
+unsigned int binary_to_uint(const char *b)
+{
+
+}
