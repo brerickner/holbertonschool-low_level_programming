cb3bc7c44b568a699cbf7211ac29b5556ac36f6e
brerickner <breannarickner@gmail.com>
2020-11-05 23:20:35 +0000

    adding comments

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * print_binary - unsigned long int n
+ * @n: number to convert to binary
+ * description: function that prints binary version of a #
+ * Return: void
+ */
+
+void print_binary(unsigned long int n)
+{
+	(void)n;
+	return;
+}
