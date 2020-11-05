a0668e35f7cbf337cfd311cddfde62c35ed68960
brerickner <breannarickner@gmail.com>
2020-11-05 23:36:12 +0000

    adding comments betty compliant

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_bit - unsigned long int *n, unsigned int index
+ * @n: value of bit
+ * @index: index of bit wanting to set
+ * description: function that sets the value of a bit to 1 at a given index
+ * Return: 1 if it worked. Else -1 if error occurs
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	return (1);
+}
