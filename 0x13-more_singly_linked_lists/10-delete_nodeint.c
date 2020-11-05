967d97494bf64836e404487203d91eb66d5d8b15
brerickner <breannarickner@gmail.com>
2020-11-05 04:57:32 +0000

    whats going on

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,5 +1,28 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * listint_t *get_nodeint_at_index - listint_t *head, unsigned int index
+ * @head: pointer to head node in listint_h
+ * @index: index of nodes in listint_t, starting at 0
+ * description: function that returns the nth node of a listint_t linked list
+ * Return: nth node of linked list. Else NULL if node does not exist
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int countNodes;
+
+/*loop through link list counting nodes*/
+	for (countNodes = 0; (index - 1) > countNodes;
+	     head = head->next, countNodes++)
+	{
+		if (!head)
+			return (NULL);
+		/*change current head pointer to next of head*/
+
+	}
+	return (head);/*if equal to index return head node*/
+}
 
 /**
  * delete_nodeint_at_index - listint_t **head, unsigned int index
@@ -12,10 +35,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *new_insert, *add;
 
-	new_insert = get_nodeint_at_index(*head, idx);
+	new_insert = get_nodeint_at_index(*head, index);
 
 	if (!new_insert || !(*head))
-		return (NULL);
+		return (-1);
 
 	add = create_node(*head);
 	add->next = (*head)->next;
