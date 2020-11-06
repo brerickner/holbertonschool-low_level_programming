5afc7c5fdd86a1ef27e6f12f02e0abbae42a370d
brerickner <breannarickner@gmail.com>
2020-11-06 02:03:00 +0000

    typo in header

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,10 +1,9 @@
 #ifndef HOLBERTON_H
-#define HOLBERTON_H_
-
-#include <stdlib.h>
+#define HOLBERTON_H
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
+
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
