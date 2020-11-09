8f5737e759d58d7ceb6ff0f47c9b1ff0fa9a1508
brerickner <breannarickner@gmail.com>
2020-11-09 22:45:02 +0000

    oops missread the fcnt1

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -4,8 +4,8 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <font1.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
