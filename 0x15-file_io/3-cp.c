d56f7d8fd42680e09afbe00c71c69f0895c9db1f
brerickner <breannarickner@gmail.com>
2020-11-10 03:42:30 +0000

    adding comments and dummy code

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * main - int argc, char argv**
+ * @argc: argument count
+ * @argv: argument vector
+ * description: program that copies contents from one file to another
+ * Return: 1 on success. Else -1 upon failure.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	(void)argv;
+
+	return (0);
+}
