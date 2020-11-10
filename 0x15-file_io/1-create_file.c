e32f7d0803512ccbc8fdcf20c03932dbf93244a5
brerickner <breannarickner@gmail.com>
2020-11-10 01:28:13 +0000

    outputting correctly it looks like

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+/**
+ * _strlen - char *s
+ * @s: string that is being measured
+ *
+ * description: function that returns lenth of string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int charCount = 0;
+
+	while (s[charCount])
+		charCount++;
+	return (charCount);
+}
+/**
+ * create_file - const char *filename, char *text_content
+ * @filename: file to be created
+ * @text_content: NULL terminated string to be printed
+ * description: create a function that creates a file.
+ * Return: 1 on success. Else -1 upon failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int theFile, writeFile, strLen;
+
+	strLen = _strlen(text_content);
+	if (filename == NULL)
+		return (-1);
+
+	theFile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (theFile == -1)
+		return (-1);
+	writeFile = write(theFile, text_content, strLen);
+	if (writeFile == -1)
+		return (-1);
+	close(theFile);
+	return (1);
+}
