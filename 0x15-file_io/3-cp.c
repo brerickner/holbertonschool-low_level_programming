c8f16d80e28c7f6b354c8826ced642a171ab9ebd
brerickner <breannarickner@gmail.com>
2020-11-11 04:24:02 +0000

    phewwwwwwww that was a doozygit add 3-cp.c git add 3-cp.c !

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,6 +4,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
+int error_func(int error, int fileD, char *theArgV);
 /**
  * main - int argc, char argv**
  * @argc: argument count
@@ -13,8 +15,67 @@
  */
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
+	char buffer[1024];
+	int theFile, theLastFile, readFile, writeFile, closingTime;
 
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit (97);
+	}
+	theFile = open(argv[1], O_RDWR);
+	if (theFile == -1)
+		error_func(98, theFile, argv[1]);
+	readFile = read(theFile, buffer, 1024);
+	if (readFile == -1)
+		error_func(99, readFile, argv[1]);
+	theLastFile = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
+	if (theLastFile == -1)
+		error_func(99, theLastFile, argv[2]);
+	while (readFile == 1024)
+	{
+		writeFile = write(theLastFile, buffer, readFile);
+		if (writeFile == -1)
+			error_func(99, writeFile, argv[2]);
+		readFile = read(theFile, buffer, 1024);
+		if (readFile == -1)
+			error_func(98, readFile, argv[2]);
+	}
+	closingTime = close(theFile);
+	if (closingTime == -1)
+		return (error_func(100, theFile, argv[1]));
+	closingTime = close(theLastFile);
+	if (closingTime == -1)
+		return (error_func(101,theLastFile, argv[2]));
+	return (0);
+}
+/**
+ * error_func - int error
+ * @error: error to print
+ * description: function with error messages
+ * Return: the error number
+ */
+int error_func(int error, int fileD, char *theArgV)
+{
+	if (error == 98)
+	{
+		dprintf(STDERR_FILENO, "Can't read from file %s\n", theArgV);
+		return (98);
+	}
+	if (error == 99)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", theArgV);
+		return (99);
+	}
+	if (error == 100)
+	{
+		dprintf(STDERR_FILENO, "Can't close %i\n", fileD);
+		return (100);
+	}
+	if (error == 101)
+	{
+		dprintf(STDERR_FILENO, "Can't close %i\n", fileD);
+		return (100);
+	}
 	return (0);
 }
