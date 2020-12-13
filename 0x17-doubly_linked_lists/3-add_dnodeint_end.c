c0c7ef4382780c06f6d5934324093d84ce623f0c
brerickner <breannarickner@gmail.com>
2020-12-13 02:22:37 +0000

    saving progress

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -2,25 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * *create_node - dlistint_t *new_node
- * @new_node: pointer to new node
- * @n: value in node
- * description: function that creates a new node
- * Return: ptr to the node created
- */
-dlistint_t *create_node(dlistint_t *new_node, const int n)
-{
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->next = NULL;
-	new_node->prev = NULL;
-	new_node->n = n;
-
-	return (new_node);
-}
-/**
  * *add_dnodeint_end - dlistint_t **head, const int n
  * @head: head node in dlistint_t
  * @n: integer value
@@ -31,20 +12,25 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *addNode, *pointy;
 
-	addNode = create_node(*head, n);
-
+	addNode = malloc(sizeof(dlistint_t));
 	if (!addNode || !head)
 		return (NULL);
+	addNode->n = n;
+	addNode->next = NULL;
+
 	if (*head == NULL)
+	{
+		addNode->prev = NULL;
 		*head = addNode;
+		return (addNode);
+	}
 	else
 	{
-		for (pointy = *head; pointy->next != NULL;
+		for (pointy = *head; pointy->next;
 		     pointy = pointy->next)
 			; /*find last node in list */
 		pointy->next = addNode; /*point to last node in list */
+		addNode->prev = pointy;
 	}
-	addNode->prev = pointy;
-
 	return (addNode);
 }
