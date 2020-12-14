83c6502a3440a86a772837ffa7a4a28f0f2e2c4a
brerickner <breannarickner@gmail.com>
2020-12-14 20:10:22 +0000

    oh m y goodness

--- 0x18-dynamic_libraries/holberton.h
+++ 0x18-dynamic_libraries/holberton.h
@@ -1,6 +1,9 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,4 +25,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* HOLBETON_H */
+#endif /* _HOLBERTON_H_*/
