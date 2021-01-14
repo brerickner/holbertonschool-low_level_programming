e2d611c726a6ea4725f3819e8fee05014361ae84
brerickner <breannarickner@gmail.com>
2021-01-14 23:36:40 +0000

    no valgrind errors now

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 /**
  * *hash_table_create - unsigned long int size
- * @size: size of hash table
+ * @size: size of the array in hash table
  * description: funtion that creates a hash table
  * Return: pointer to newly created hash table. Else NULL
  */
@@ -17,14 +17,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	newHash->size = size;
-	newHash->array = malloc(sizeof(hash_table_t *) * size);
+	newHash->array = malloc(sizeof(hash_node_t *) * size);
 	if (!newHash->array)
 	{
 		free(newHash);
 		return (NULL);
 	}
 	/* make all entries empty by setting each pointer to point to NULL */
-	for (index = 0; index < size; index++, newHash->array[index] = NULL)
+	for (index = 0; index < size; newHash->array[index] = NULL, index++)
 		;
 	return (newHash);
 }
