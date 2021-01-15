4d00d14bd6e0c92e475ecbd9d9a63f10d5018649
brerickner <breannarickner@gmail.com>
2021-01-15 20:38:02 +0000

    took out frees to save room, they were not doing anything

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,15 +10,15 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int keyIndex, index;
+	unsigned long int index;
 	hash_node_t *newNode = NULL;
 
 	/* make copy of value and then check for memory allocation */
 	if (!value || !key || !ht || strcmp(key, "") == 0 || strlen(key) == 0)
 		return (0);
 	/* use hash function to get new key index*/
-	index = keyIndex = key_index((unsigned char *)key, ht->size);
-	/* go through adding or updating key/value pair in keyIndex location */
+	index = key_index((unsigned char *)key, ht->size);
+	/* go through adding or updating key/value pair in index location */
 	for (; ht->array[index]; index++)
 	{
 	/* if keys match get rid of what's in there and replace with new Value */
@@ -30,24 +30,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				return (0);
 			return (1);
 		}
+		ht->array[index] = ht->array[index]->next;
 	}
 	/* else create newNode to start storing key/value pairs */
 	newNode = malloc(sizeof(hash_node_t));
 	if (!newNode)
 		return (0);
-	newNode->next = NULL;
+	newNode->next = ht->array[index];
 	newNode->key = strdup(key);
 	if (!newNode->key)
-	{
-		free(newNode);
 		return (0);
-	}
 	newNode->value = strdup(value);
 	if (!newNode->value)
-	{
-		free(newNode);
 		return (0);
-	}
-	ht->array[keyIndex] = newNode;
+	ht->array[index] = newNode;
 	return (1);
 }
