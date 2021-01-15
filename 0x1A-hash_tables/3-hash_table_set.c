54ea90dcffda78067f886436b70dea15e14e9a36
brerickner <breannarickner@gmail.com>
2021-01-15 20:27:23 +0000

    missing value to check if value is null

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -26,7 +26,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			free(ht->array[index]->value);
 			ht->array[index]->value = strdup(value);
-			if (!ht->array[index])
+			if (!ht->array[index]->value)
 				return (0);
 			return (1);
 		}
