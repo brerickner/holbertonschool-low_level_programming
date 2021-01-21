fa45b1f45fa4ebfb0b5fcb11ebe40579247fc194
brerickner <breannarickner@gmail.com>
2021-01-21 16:11:43 +0000

    test test test

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,7 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				return (0);
 			return (1);
 		}
-		ht->array[index] = ht->array[index]->next;
+		/*ht->array[index]->next = ht->array[index]->next->next;*/
 	}
 	/* else create newNode to start storing key/value pairs */
 	newNode = malloc(sizeof(hash_node_t));
