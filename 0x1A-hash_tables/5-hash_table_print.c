452a1a4e022b5b424af6f570fb4fc555bcac9978
brerickner <breannarickner@gmail.com>
2021-01-21 16:37:38 +0000

    changed flag to 1 and not ++

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -21,10 +21,8 @@ void hash_table_print(const hash_table_t *ht)
 			if (flag)
 				printf(", ");
 			printf("'%s' : '%s'", hashPass->key, hashPass->value);
-			
+			flag++;
 		}
-		flag++;
 	}
-
 	printf("}\n");
 }
